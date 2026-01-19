39dcb1a4739de1703eddd65a7cde9eb35fae1eea
osephson <osefson@gmail.com>
2026-01-19 06:29:10 -0500

    fix: prisma configuration

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -5,7 +5,7 @@ import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+    const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL || 'file:./dev.db' });
     super({ adapter });
   }
 }
